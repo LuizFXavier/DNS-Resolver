7985facd2ff7c4b826511ca56da66bc631442579
LuizFXavier <zap422238@gmail.com>
2025-10-09 04:10:21 -0400

    feat: Adicionado método para impressão dos dados.

--- src/model/DnsMessage.java
+++ src/model/DnsMessage.java
@@ -7,12 +7,51 @@ import model.dnsStructure.RR;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class DnsMessage {
+public class DnsMessage {
     DnsHeader header;
     List<Question> questions = new ArrayList<Question>();
     List<RR> answers = new ArrayList<RR>();
     List<RR> authorities = new ArrayList<RR>();
     List<RR> additional = new ArrayList<RR>();
+
+    public DnsMessage(DnsHeader header){
+        this.header = header;
+    }
+
+    @Override
+    public String toString(){
+        String h = header.toString();
+        String quest = "";
+        String answ = "";
+        String auth = "";
+        String addi = "";
+
+        if(header.questions() > 0){
+            quest = "\nQuest:\n";
+            for(int i = 0; i < header.questions(); i++){
+                quest = quest.concat("       " + questions.get(i).toString()) + "\n";
+            }
+        }
+        if(header.answers() > 0){
+            answ = "\nAnswer:\n";
+            for(int i = 0; i < header.answers(); i++){
+                answ = answ.concat("       " + answers.get(i).toString()) + "\n";
+            }
+        }
+        if(header.authorities() > 0){
+            auth = "\nAuth:\n";
+            for(int i = 0; i < header.authorities(); i++){
+                auth = auth.concat("      " + authorities.get(i).toString()) + "\n";
+            }
+        }
+        if(header.additional() > 0){
+            addi = "\nAdd:\n";
+            for(int i = 0; i < header.additional(); i++){
+                addi = addi.concat("     " + additional.get(i).toString()) + "\n";
+            }
+        }
+        return h + quest + answ + auth + addi;
+    }
 
     public DnsHeader header() {
         return header;
@@ -32,6 +71,22 @@ public abstract class DnsMessage {
 
     public List<RR> additional() {
         return additional;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public void setAnswers(List<RR> answers) {
+        this.answers = answers;
+    }
+
+    public void setAuthorities(List<RR> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void setAdditional(List<RR> additional) {
+        this.additional = additional;
     }
 }
 
