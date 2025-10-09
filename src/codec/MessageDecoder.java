288ec8b36ea107af647acb2bfbf6dc89a8e7d221
LuizFXavier <zap422238@gmail.com>
2025-10-09 04:11:50 -0400

    feat: Completude do decodificador de mensagens DNS.

--- src/codec/MessageDecoder.java
+++ src/codec/MessageDecoder.java
@@ -2,12 +2,16 @@ package codec;
 
 import model.DnsMessage;
 import model.dnsStructure.DnsHeader;
+import model.dnsStructure.Question;
+import model.dnsStructure.RR;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessageDecoder {
 
-    DnsHeader decodeHeader(byte[] bytes) {
+    private static DnsHeader decodeHeader(byte[] bytes) {
         DnsHeader header = new DnsHeader();
         ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
@@ -20,13 +24,84 @@ public class MessageDecoder {
         return header;
     }
 
-    public void decode(byte[] bytes) {
+    private static String decodeName(ByteBuffer buffer) {
+
+        byte b = buffer.get();
+        ArrayList<Character> chars = new ArrayList<>();
+
+        while (b != (byte) 0x00) {
+            for (byte i = 0; i < b; ++i) {
+                chars.add((char) buffer.get());
+            }
+            chars.add('.');
+            b = buffer.get();
+        }
+        chars.removeLast();
+
+        StringBuilder sb = new StringBuilder(chars.size());
+
+        for (Character c : chars) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
+    private static Question decodeQuestion(ByteBuffer buffer) {
+
+        String name = decodeName(buffer);
+
+        short QTYPE = buffer.getShort();
+        short QCLASS = buffer.getShort();
+
+        return new Question(name, QTYPE, QCLASS);
+    }
+
+    private static List<Question> decodeQuestions(short n, ByteBuffer buffer) {
+
+        ArrayList<Question> questions = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            questions.add(decodeQuestion(buffer));
+        }
+        return questions;
+    }
+    private static RR decodeRR(ByteBuffer buffer) {
+
+        String NAME = decodeName(buffer);
+        short TYPE = buffer.getShort();
+        short CLASS = buffer.getShort();
+        int TTL = buffer.getInt();
+        short RDLENGHT = buffer.getShort();
+
+        byte[] RDATA = new byte[RDLENGHT];
+        buffer.get(RDATA);
+
+        return new RR(NAME, TYPE, CLASS, TTL, RDLENGHT, RDATA);
+    }
+
+    private static List<RR> decodeRRs(int n, ByteBuffer buffer) {
+        ArrayList<RR> RRs = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            RRs.add(decodeRR(buffer));
+        }
+        return RRs;
+    }
+    public static DnsMessage decode(byte[] bytes) {
         ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
         byte[] headerBytes = new byte[DnsHeader.BYTES()];
         buffer.get(headerBytes);
 
         DnsHeader header = decodeHeader(headerBytes);
-        
+        DnsMessage message = new DnsMessage(header);
+
+        message.setQuestions(decodeQuestions(header.questions(), buffer));
+        message.setAnswers(decodeRRs(header.answers(), buffer));
+        message.setAuthorities(decodeRRs(header.authorities(), buffer));
+        message.setAdditional(decodeRRs(header.additional(), buffer));
+
+        return message;
     }
 }
